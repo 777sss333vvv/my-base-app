406a8af798738ec5d276efb63af28ba87cd3de7a
777sss333vvv <UserBox2@outlook.com>
2026-01-24 18:23:24 +0100

    fix: add tailwind directives

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",      // Если папка app в корне
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",  // Если папка app внутри src
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@coinbase/onchainkit/**/*.{js,ts,jsx,tsx,mdx}", // Добавь эту строку!
   ],
   theme: {
     extend: {
